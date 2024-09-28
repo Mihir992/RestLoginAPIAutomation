99708fa7cbb3b9fd417a93600e0f57b9deae5814
Mihir992 <119054373+Mihir992@users.noreply.github.com>
2024-09-28 22:44:02 +0530

    Update LoginEndpoint.java

--- src/main/java/com/qa/endpoints/LoginEndpoint.java
+++ src/main/java/com/qa/endpoints/LoginEndpoint.java
@@ -1,5 +1,8 @@
 package com.qa.endpoints;
 import static io.restassured.RestAssured.given;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.parsing.Parser;
 import io.restassured.response.Response;
 import java.util.Base64;
 import io.restassured.path.json.JsonPath;
@@ -8,14 +11,16 @@ public class LoginEndpoint {
     public static String performLogin(String username, String password){
         // Base64 encode the username and password
         String credentials = Base64.getEncoder().encodeToString((username + ":" + password).getBytes());
-        Response response = given()
+        Response response=given()
                 .header("Authorization", "Basic " + credentials) // Use Basic Authentication
                 .header("Content-Type", "application/json")
                 .post(Routes.login_url);
 
         response.then().statusCode(200); // Ensure the response status is OK (200
         // Return the token from the response
-        String token = response.jsonPath().getString("token");
+        // Extract the token using JsonPath
+        JsonPath jsonPath = response.jsonPath();
+        String token = jsonPath.getString("token");
         return token;
     }
 }
