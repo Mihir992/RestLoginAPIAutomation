13303134c1412e75a257296963ed8e1330b5e2ff
Mihir992 <119054373+Mihir992@users.noreply.github.com>
2024-09-29 09:50:51 +0530

    Update Routes.java

--- src/main/java/com/qa/endpoints/Routes.java
+++ src/main/java/com/qa/endpoints/Routes.java
@@ -2,7 +2,7 @@ package com.qa.endpoints;
 
 public class Routes {
 
-    public static String base_url = "https://fakeapi.platzi.com/en/rest/auth-jwt/";
+    public static String base_url = "https://api.escuelajs.co/api/v1/";
 
     //Login module
     public static String login_url = base_url+"/auth/login";
