7bf99b46af30075a156e03d01605b426367421ce
Mihir992 <119054373+Mihir992@users.noreply.github.com>
2024-09-29 11:02:22 +0530

    UpdateTestLoginAPI-29-09-2024.java

--- src/test/java/testScript/TestLoginAPI.java
+++ src/test/java/testScript/TestLoginAPI.java
@@ -2,8 +2,11 @@ package testScript;
 
 import com.qa.endpoints.LoginEndpoint;
 import com.qa.utils.ExcelUtils;
+import com.qa.utils.ExtentReport;
 import com.qa.utils.FileUtils;
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import java.io.IOException;
@@ -11,6 +14,12 @@ import java.io.IOException;
 import static com.qa.utils.FileUtils.saveTokenToFile;
 
 public class TestLoginAPI {
+
+    @BeforeClass
+    public void setUp(){
+        ExtentReport.initReports();
+    }
+
     @DataProvider(name = "loginData")
     public Object[][] loginDataProvider() throws IOException {
         // Call the modified method to get data as String[][]
@@ -18,12 +27,21 @@ public class TestLoginAPI {
     }
 
     @Test(dataProvider = "loginData")
-    public void testLogin(String username, String password) throws IOException {
-        LoginEndpoint loginEndpoint = new LoginEndpoint();
-        String token = loginEndpoint.performLogin(username, password);
-       // Assert.assertNotNull(token, "Token should not be null");
-        // Save the token to a file
-        saveTokenToFile(token);
+    public void testLogin(String email, String password) throws IOException {
+        try
+        {
+            LoginEndpoint loginEndpoint = new LoginEndpoint();
+            String token = loginEndpoint.performLogin(email, password);
+            System.out.println("Login successful, token: " + token);
+            saveTokenToFile(token);
+        } catch (AssertionError e) {
+            System.err.println("Login failed: " + e.getMessage());
+        }
+    }
+
+    @AfterClass
+    public void tearDown(){
+        ExtentReport.flushReports();
     }
 }
 
