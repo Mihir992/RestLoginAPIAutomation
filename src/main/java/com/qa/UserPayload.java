c8362b29ec41a236e84712417e6bf2984f663dc8
Mihir992 <119054373+Mihir992@users.noreply.github.com>
2024-10-11 15:28:43 +0530

    Add files via upload

---
+++ src/main/java/com/qa/UserPayload.java
@@ -0,0 +1,26 @@
+package com.qa.payload;
+
+public class UserPayload {
+   // int id;
+    String name;
+    String job;
+
+    /*public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }*/
+    public String getname() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getjob() {
+        return job;
+    }
+    public void setJob(String job) {
+        this.job = job;
+    }
+}
